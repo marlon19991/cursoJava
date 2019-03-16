f85a8b3be9dcf7d1fa268b5a1a77b943f1438919
Marlon <esteban19991@gmail.com>
2019-03-15 19:06:35 -0500

    borrado

--- PrimerosPasos/src/paquetepruebas/Clase3.java
+++ PrimerosPasos/src/paquetepruebas/Clase3.java
@@ -6,9 +6,6 @@ public class Clase3 extends Clase1{
 	
     int g =335;
     String gat="uu";
-    String gadt="uu";
-    String gfft="uudssd";
-    int hyu = 45;
-    String comin = "Esto es un comentario";
+    
     
 }
